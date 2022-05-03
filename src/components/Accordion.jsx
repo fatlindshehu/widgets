be224e73844a686d7cd05ae3a14d68aa257e8b5a
Fatlind Shehu <fatlindshehu4@gmail.com>
2022-05-03 19:05:03 +0100

    Using helper functions to structure code

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,22 +1,18 @@
-import React, { useState } from 'react';
+import React from "react";
 
 const Accordion = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
-
-  const onTitleClick = (index) => {
-    setActiveIndex(index);
+  const handleClick = (indexus) => {
+    console.log("Title has been clicked", indexus);
   };
 
   const renderedItems = items.map((item, index) => {
-    const active = index === activeIndex ? 'active' : '';
-
     return (
       <React.Fragment key={item.title}>
-        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+        <div className="title active" onClick={() => handleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className={`content ${active}`}>
+        <div className="content active">
           <p>{item.content}</p>
         </div>
       </React.Fragment>
